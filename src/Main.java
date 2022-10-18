f1ba07de56e0d7025395a515b20c996bdfdc8e38
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2022-10-18 19:35:00 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main{
+    static class Sat {
+        int sati,minute,sekunde;
+        public
+        Sat(int sati, int minute, int sekunde) { Postavi(sati,minute,sekunde); }
+        void Postavi(int sati, int minute, int sekunde) { this.sati=sati; this.minute=minute; this.sekunde=sekunde; }
+        void Sljedeci() {
+            sekunde++;
+            if (sekunde==60) { sekunde=0; minute++; }
+            if (minute==60) { minute=0; sati++; }
+            if (sati==24) sati=0;
+        }
+        void Prethodni() {
+            sekunde--;
+            if (sekunde==-1) { sekunde=59; minute--; }
+            if (minute==-1) { minute=59; sati--; }
+            if (sati==-1) sati=23;
+        }
+        void PomjeriZa(int pomak) {
+            if (pomak>0) for (int i = 0; i<pomak; i++) Sljedeci();
+		else for (int i = 0; i<-pomak; i++) Prethodni();
+        }
+        int DajSate() { return sati; }
+        int DajMinute() { return minute; }
+        int DajSekunde() { return sekunde; }
+        void Ispisi() {System.out.println(sati + ":" + minute + ":" + sekunde); }
+    };
+
+    public static void main(String[] args){
+        Sat s = new Sat(15,30,45);
+        s.Ispisi();
+        s.Sljedeci();
+        s.Ispisi();
+        s.PomjeriZa(-48);
+        s.Ispisi();
+        s.Postavi(0,0,0);
+        s.Ispisi();
+    }
+
+}
